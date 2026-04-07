d1f535fd80d527b2178e70990eddea355d9dce9a
IfatHemal <ifathemal9@gmail.com>
2026-04-08 01:16:13 +0600

    User Login UI Added

--- app/src/main/java/com/enumtech/healthline/LoginActivity.java
+++ app/src/main/java/com/enumtech/healthline/LoginActivity.java
@@ -13,35 +13,35 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class activity_register extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_register);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.register), (v, insets) -> {
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-
         Spinner spinner = findViewById(R.id.spinnerRole);
         String[] roles = {"Patient", "Doctor", "Blood Donor", "Ambulance Driver"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                activity_register.this, android.R.layout.simple_spinner_dropdown_item, roles);
+                LoginActivity.this, android.R.layout.simple_spinner_dropdown_item, roles);
         adapter.setDropDownViewResource(
                 android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
 
-        TextView tvLogin = findViewById(R.id.tvLogin);
-        tvLogin.setOnClickListener(new View.OnClickListener() {
+        TextView tvSignup = findViewById(R.id.tvSignup);
+        tvSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               startActivity(new Intent(activity_register.this, LoginActivity.class));
+                startActivity(new Intent(LoginActivity.this,activity_register.class));
             }
         });
+
     }
 }
